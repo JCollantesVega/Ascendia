6a4905ba669a2da3d9535932649e34f5fe8844d6
JCollantesVega <collantes.jantonio@gmail.com>
2026-01-30 19:41:09 +0100

    Initial commit and project setup

---
+++ backend/src/main/java/io/JCollantesVega/Ascendia/AscendiaApplication.java
@@ -0,0 +1,13 @@
+package io.JCollantesVega.Ascendia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscendiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AscendiaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/JCollantesVega/Ascendia/AscendiaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.JCollantesVega.Ascendia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscendiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
