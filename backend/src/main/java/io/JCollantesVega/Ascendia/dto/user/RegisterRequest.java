37a0593fd1b3eac240e42a873eab3ba97fd6b75e
JCollantesVega <collantes.jantonio@gmail.com>
2026-01-31 04:44:47 +0100

    Add: AuthService and UserDetails implementation

--- backend/src/main/java/io/JCollantesVega/Ascendia/dto/user/RegisterRequest.java
+++ backend/src/main/java/io/JCollantesVega/Ascendia/dto/user/RegisterRequest.java
@@ -20,4 +20,20 @@ public record RegisterRequest (
     public String getEmail() {
         return email;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
 }
