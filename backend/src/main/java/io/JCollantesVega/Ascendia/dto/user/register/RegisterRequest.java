6a14f618a8e94f6ce47f943cdb30aa380a6a17c7
JCollantesVega <collantes.jantonio@gmail.com>
2026-02-01 19:44:04 +0100

    Add: Auth DTOs and fix DTO directory

---
+++ backend/src/main/java/io/JCollantesVega/Ascendia/dto/user/register/RegisterRequest.java
@@ -0,0 +1,40 @@
+package io.JCollantesVega.Ascendia.dto.user.register;
+
+import io.JCollantesVega.Ascendia.Enum.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+
+public record RegisterRequest (
+    @NotBlank(message = "Username is required")
+    String username,
+    @NotBlank(message = "Email is required")
+    String email,
+    @NotBlank(message = "Password is required")
+    String password,
+    @NotNull(message = "Role is required")
+    Role role,
+
+    String avatarUrl
+){
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+}
