fa9953778216b93718057fde9fd1f986a804a121
JCollantesVega <collantes.jantonio@gmail.com>
2026-02-01 22:18:40 +0100

    Fix: create ApplicationConfig, implementing neccesary separation of concerns

--- backend/src/main/java/io/JCollantesVega/Ascendia/config/SecurityConfig.java
+++ backend/src/main/java/io/JCollantesVega/Ascendia/config/SecurityConfig.java
@@ -2,21 +2,13 @@ package io.JCollantesVega.Ascendia.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import io.JCollantesVega.Ascendia.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -42,31 +34,5 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-
-    // Configuración para autenticación
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
-
-    // Configuración para autenticación
-    @Bean
-    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder);
-        return authProvider;
-    }
-
-    // Configuración para autenticación
-    @Bean
-    public UserDetailsService userDetailsService(UserRepository repository) {
-        return username -> repository.findByEmail(username)
-            .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
-    }
+    
 }
