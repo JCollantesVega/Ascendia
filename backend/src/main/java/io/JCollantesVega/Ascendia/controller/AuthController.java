7d0d9b5a0c45d87f379ae998c4c94961b57492c4
JCollantesVega <collantes.jantonio@gmail.com>
2026-02-01 21:54:43 +0100

    Add: login endpoint

--- backend/src/main/java/io/JCollantesVega/Ascendia/controller/AuthController.java
+++ backend/src/main/java/io/JCollantesVega/Ascendia/controller/AuthController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.JCollantesVega.Ascendia.dto.user.auth.AuthRequest;
+import io.JCollantesVega.Ascendia.dto.user.auth.AuthResponse;
 import io.JCollantesVega.Ascendia.dto.user.register.RegisterRequest;
 import io.JCollantesVega.Ascendia.service.AuthService;
 import jakarta.validation.Valid;
@@ -22,5 +24,11 @@ public class AuthController {
     public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest request) {
         authService.register(request);
         return new ResponseEntity<>("Usuario registrado exitosamente", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody AuthRequest request) {
+        AuthResponse response = authService.login(request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
