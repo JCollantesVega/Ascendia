6badb997c604955ea369b6e60108dac00ec4b89c
JCollantesVega <collantes.jantonio@gmail.com>
2026-02-10 11:45:26 +0100

    Add: mentor data mocks

---
+++ client/src/features/mentors/mocks/mentorData.ts
@@ -0,0 +1,19 @@
+import type { MentorCardProps } from "../../../types/mentor";
+
+export const mentorCards:MentorCardProps[] = [
+    {
+        name: "John",
+        speciality: "e-commerce",
+        price: 50
+    },
+    {
+        name: "Jordan Belfort",
+        speciality: "Crypto, Trading",
+        price: 60
+    },
+    {
+        name: "Jorge",
+        speciality: "Individual growth, Personal Branding",
+        price: 55
+    }
+]
